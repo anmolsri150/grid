70e00fa563eaaa5c4f4b1b5461505b47a1c32e5d
Anmol Srivastava <anmol150srivastava@gmail.com>
2021-09-27 22:12:18 +0530

    chatbot init

---
+++ src/components/chatbot.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="chatbot">
+    <div class="chatbot-header">
+      HEADER
+    </div>
+    <div class="chatbot-body">
+      BODY
+    </div>
+    <div class="chatbot-footer">
+      FOOTER
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "chatbot"
+}
+</script>
+
+<style>
+.chatbot {
+  position: absolute;
+  right: 1rem;
+  bottom: 1rem;
+  background-color: lightgrey;
+  min-width: 175px;
+  min-height: 300px;
+  width: 300px;
+  height: 500px;
+}
+.bottom-left {
+
+}
+</style>
+
+<style scoped>
+</style>
